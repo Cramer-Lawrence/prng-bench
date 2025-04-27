3e262e175ecf2ee2779488c2d6d7dc31621647cc
Cramer-Lawrence <cramer.lawrence@msn.com>
2025-04-27 09:32:27 -0600

    Updated the PCG32 to be thread-safe when not thread local

--- include/prng/pcg32.hpp
+++ include/prng/pcg32.hpp
@@ -12,11 +12,10 @@ public:
     uint32_t next32();
 
 private:
-    uint64_t state;
-    uint64_t inc;
+    std::atomic<uint64_t> m_inc;
 
-    const uint64_t multiplier {6364136223846793005ULL}; // from pcg32 research paper
-    const uint64_t incrementalValue {0xda3e39cb94b95bdbULL};
+    const uint64_t m_multiplier {6364136223846793005ULL}; // from pcg32 research paper
+    const uint64_t m_incrementalValue {0xda3e39cb94b95bdbULL};
 };
 
 #endif
--- src/prng/pcg32.cpp
+++ src/prng/pcg32.cpp
@@ -3,17 +3,21 @@
 
 void PCG32::seedImpl(uint64_t inSeed) {
     
-    state = 0U;
-    inc = (incrementalValue << 10) | 1U;
+    m_seed.store(0U, std::memory_order_relaxed);
+    m_inc.store(((m_incrementalValue << 10) | 1U), std::memory_order_relaxed);
     next();
-    state += inSeed;
+    m_seed.fetch_add(inSeed, std::memory_order_relaxed);
     next();
 }
 
 uint32_t PCG32::next32() {
 
-    uint64_t oldState = state;
-    state = oldState * multiplier + inc;
+    uint64_t oldState = m_seed.load(std::memory_order_relaxed);
+    uint64_t newState {};
+
+    do {
+        newState = oldState * m_multiplier + m_inc.load(std::memory_order_relaxed);
+    } while (!m_seed.compare_exchange_weak(oldState, newState, std::memory_order_relaxed));
 
     uint32_t xorshifted {static_cast<uint32_t>(((oldState >> 18u) ^ oldState) >> 27u)};
     uint32_t rotation {static_cast<uint32_t>(oldState >> 59u)};
